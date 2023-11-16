9768d488b246326484dfa66d49d7ad58bde9563a
landris006 <laczko.andris007@gmail.com>
2023-11-17 00:31:54 +0100

    use system clipboard

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,6 +3,8 @@ vim.g.mapleader          = " "
 vim.g.loaded_netrw       = 1
 vim.g.loaded_netrwPlugin = 1
 
+vim.opt.clipboard        = "unnamedplus"
+
 vim.opt.cursorline       = true
 vim.opt.number           = true
 vim.opt.relativenumber   = true
