2a23ede7e965df65ca94df245da95d4cc32c037d
landris006 <laczko.andris007@gmail.com>
2023-12-01 18:28:24 +0100

    wgsl filetype autocmd

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -36,3 +36,18 @@ vim.api.nvim_create_autocmd("ColorScheme", {
     end
   end,
 })
+
+vim.api.nvim_create_autocmd(
+  {
+    "BufNewFile",
+    "BufRead",
+  },
+  {
+    pattern = "*.wgsl",
+    callback = function()
+      vim.bo.filetype = "wgsl"
+      -- local buf = vim.api.nvim_get_current_buf()
+      -- vim.api.nvim_buf_set_option(buf, "filetype", "wgsl")
+    end
+  }
+)
