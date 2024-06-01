5a9231a22db7a48aa1edc9d5cfd5956c363bdfe1
landris006 <laczko.andris007@gmail.com>
2024-06-01 10:25:14 +0200

    rasi filetype

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -51,3 +51,18 @@ vim.api.nvim_create_autocmd(
     end
   }
 )
+
+vim.api.nvim_create_autocmd(
+  {
+    "BufNewFile",
+    "BufRead",
+  },
+  {
+    pattern = "*.rasi",
+    callback = function()
+      vim.bo.filetype = "rasi"
+      -- local buf = vim.api.nvim_get_current_buf()
+      -- vim.api.nvim_buf_set_option(buf, "filetype", "wgsl")
+    end
+  }
+)
