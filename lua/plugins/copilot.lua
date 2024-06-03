60a7af314d103cc96a536ee5f2df36d5f9b11790
landris006 <laczko.andris007@gmail.com>
2024-06-03 23:39:10 +0200

    super maven

---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    "github/copilot.vim",
+    event = "BufReadPost",
+    enabled = false,
+    init = function()
+      vim.g.copilot_no_tab_map = true
+      vim.g.copilot_assume_mapped = true
+      vim.g.copilot_tab_fallback = "<S-Tab>"
+    end
+  },
+  {
+    "supermaven-inc/supermaven-nvim",
+    config = function()
+      require("supermaven-nvim").setup({
+        keymaps = {
+          accept_suggestion = "<S-Tab>",
+        },
+      })
+    end,
+  },
+}
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -173,15 +173,5 @@ return {
         }
       })
     end
-  },
-  {
-    "github/copilot.vim",
-
-    event = "BufReadPost",
-    init = function()
-      vim.g.copilot_no_tab_map = true
-      vim.g.copilot_assume_mapped = true
-      vim.g.copilot_tab_fallback = "<S-Tab>"
-    end
   }
 }
