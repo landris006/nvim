9c70907b29aed442800dfdae6161f1be3fdc3b9e
landris006 <laczko.andris007@gmail.com>
2025-01-02 19:41:42 +0100

    deprecation fix

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,5 +1,5 @@
 local function attached_clients()
-	local buf_clients = vim.lsp.get_active_clients({ bufnr = 0 })
+	local buf_clients = vim.lsp.get_clients({ bufnr = 0 })
 	if #buf_clients == 0 then
 		return "LSP Inactive"
 	end
