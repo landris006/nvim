0cbdb1d2d719c9dbde684c7edc0fe112626e215e
landris006 <laczko.andris007@gmail.com>
2025-07-22 18:16:29 +0200

    formatting

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -1,33 +1,34 @@
 return {
-	"mfussenegger/nvim-lint",
+  "mfussenegger/nvim-lint",
 
-	event = { "BufReadPost", "BufNewFile" },
-	opts = {
-		events = { "BufReadPost", "BufWritePost", "InsertLeave", "BufNewFile" },
-		linters_by_ft = {
-			lua = { "luacheck" },
-			c = { "cpplint" },
-			cpp = { "cpplint" },
-			python = { "mypy" },
-			go = { "golangcilint" },
-			-- rust = { "clippy" },
-			-- javascript = { "eslint_d", "eslint" },
-			-- typescript = { "eslint_d", "eslint" },
-			-- html = { "eslint_d", "eslint" },
-		},
-	},
-	config = function(_, opts)
-		require("lint").linters_by_ft = opts.linters_by_ft
+  event = { "BufReadPost", "BufNewFile" },
+  opts = {
+    events = { "BufReadPost", "BufWritePost", "InsertLeave", "BufNewFile" },
+    linters_by_ft = {
+      lua = { "luacheck" },
+      c = { "cpplint" },
+      cpp = { "cpplint" },
+      python = { "mypy" },
+      go = { "golangcilint" },
+      -- rust = { "clippy" },
+      -- javascript = { "eslint_d", "eslint" },
+      -- typescript = { "eslint_d", "eslint" },
+      -- htmlangular = { "eslint_d", "eslint" },
+      -- html = { "eslint_d", "eslint" },
+    },
+  },
+  config = function(_, opts)
+    require("lint").linters_by_ft = opts.linters_by_ft
 
-		local lint_autogroup = vim.api.nvim_create_augroup("lint", { clear = true })
+    local lint_autogroup = vim.api.nvim_create_augroup("lint", { clear = true })
 
-		vim.api.nvim_create_autocmd(opts.events, {
-			group = lint_autogroup,
-			callback = function()
-				require("config.utils").debounce(100, function()
-					require("lint").try_lint()
-				end)()
-			end,
-		})
-	end,
+    vim.api.nvim_create_autocmd(opts.events, {
+      group = lint_autogroup,
+      callback = function()
+        require("config.utils").debounce(100, function()
+          require("lint").try_lint()
+        end)()
+      end,
+    })
+  end,
 }
